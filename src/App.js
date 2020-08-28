f66b36fda2861e12143cd190619b3c52c69f11f0
Iftesum-Zami <tmizami20@gmail.com>
2020-08-28 07:06:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <h1>Hello React</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
